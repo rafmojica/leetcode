0aed7cb95e7ce67608a2909edaac2f16b1bda76a
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-04-22 16:45:06 -0700

    0125-VALID-PALINDROME Stats: Time: 3 ms (98.97%), Space: 20.6 MB (25.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,12 +3,16 @@ class Solution:
         s_new = re.sub(r'[^a-zA-Z0-9]+', '', s)
         s_new = s_new.lower()
 
-        l, r = 0, len(s_new) - 1
+        if not s_new:
+            return True
 
-        while l < r:
-            if s_new[l] == s_new[r]:
-                l += 1
-                r -= 1
-            else:
-                return False
-        return True
+        # two pointers
+        l = 0
+        r = len(s_new) - 1
+
+        while s_new[l] == s_new[r] and l < r:
+            l += 1
+            r -= 1
+
+        return l >= r
+        
