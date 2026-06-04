1777ba202196a7a4c60d329b875959f5475d3902
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-06-04 16:31:47 -0700

    2358-NUMBER-OF-WAYS-TO-SPLIT-ARRAY Stats: Time: 44 ms (94.74%), Space: 32.1 MB (89.59%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -1,16 +1,13 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        # build prefix sum
-        # left section = prefix[i]
-        # right section = prefix[-1] - left section
-        prefix = [nums[0]]
-        ans = 0
-        for i in range(1, len(nums)):
-            prefix.append(nums[i] + prefix[-1])
+        # build left section on the fly
+        # right section = sum(nums) - left section
+        ans = left_section = 0
+        sumNums = sum(nums) # O(n) time
 
         for i in range(len(nums) - 1):
-            left_section = prefix[i]
-            right_section = prefix[-1] - left_section
+            left_section += nums[i]
+            right_section = sumNums - left_section
 
             if left_section >= right_section:
                 ans += 1
