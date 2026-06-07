89db76275908ea57d96d2cac11530c495f6d661a
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-06-07 15:27:12 -0700

    1391-COUNTING-ELEMENTS Stats: Time: 0 ms (100%), Space: 19.2 MB (71.57%) - LeetHub

--- 1391-counting-elements/1391-counting-elements.py
+++ 1391-counting-elements/1391-counting-elements.py
@@ -1,17 +1,10 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
-        # use hashmap to check if element exists in O(1) time
-        # if x + 1 already exists in the set, add 1 to output
-        # two for loops? O(2n) time
-        
-        dic = {}
+        seen = set(arr)
         output = 0
-        for i, v in enumerate(arr):
-            dic[v] = i
-            
-        for num in arr:
-            if num + 1 in dic:
+        for x in arr:
+            if x + 1 in seen:
                 output += 1
-        
+
         return output
         
