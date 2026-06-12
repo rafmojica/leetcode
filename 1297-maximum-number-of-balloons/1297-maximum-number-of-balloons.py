496892399127bb14956748e4216b127ed9374b43
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-06-11 21:23:49 -0700

    1297-MAXIMUM-NUMBER-OF-BALLOONS Stats: Time: 3 ms (62.57%), Space: 19.3 MB (76%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -3,7 +3,7 @@ class Solution:
         # create hashmap counting the num of characters.
         # 1 b, 1 a, 2 l, 2 o, 1 n
         freq = {}
-        bCount, aCount, lCount, oCount, nCount, finalCount = 0, 0, 0, 0, 0, 0
+        bCount, aCount, lCount, oCount, nCount = 0, 0, 0, 0, 0
         for c in text:
             freq[c] = freq.get(c, 0) + 1
             
@@ -18,14 +18,6 @@ class Solution:
                 oCount = freq.get(letter)
             if letter == 'n':
                 nCount = freq.get(letter)
-                
-        while bCount >= 1 and aCount >= 1 and lCount >= 2 and oCount >= 2 and nCount >= 1:
-            bCount -= 1
-            aCount -= 1
-            lCount -= 2
-            oCount -= 2
-            nCount -= 1
-            finalCount += 1
             
-        return finalCount
+        return min(bCount, aCount, lCount // 2, oCount // 2, nCount)
         
