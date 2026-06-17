174244affa1faa1136742d80b123b7b23299a528
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-06-17 11:43:26 -0700

    0383-RANSOM-NOTE Stats: Time: 23 ms (53.46%), Space: 19.5 MB (72.08%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,13 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # create a map for ransomNote and magazine respectively
         count = [0] * 26
         for c in magazine:
             count[ord(c) - ord('a')] += 1
 
         for c in ransomNote:
-            count[ord(c) - ord('a')] -= 1
-            if count[ord(c) - ord('a')] < 0:
+            idx = ord(c) - ord('a')
+            count[idx] -= 1
+            
+            if count[idx] < 0:
                 return False
         
         return True
