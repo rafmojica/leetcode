f741bce20d45fa7de43cb5fb1578f7e2dca9b48f
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-06-17 10:18:55 -0700

    0049-GROUP-ANAGRAMS Stats: Time: 15 ms (43.79%), Space: 24 MB (17.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # sort each string
-        # map<string, list>
-        # append to list value to each corresponding key
-        # return list(map.values())?
-        anagrams = {}
+        freq = {}
         for s in strs:
-            key = "".join(sorted(s))
-            if key in anagrams:
-                anagrams.get(key).append(s)
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            letterFreq = tuple(count)
+            if letterFreq in freq:
+                freq.get(letterFreq).append(s)
             else:
-                anagrams[key] = [s]
+                freq[letterFreq] = [s]
 
-        return list(anagrams.values())
+        return list(freq.values())
