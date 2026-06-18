0b4394871033bffdd6fe464b3141ceffbded8744
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-06-17 17:06:13 -0700

    0525-CONTIGUOUS-ARRAY Stats: Time: 67 ms (98.01%), Space: 25.6 MB (8.57%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,19 +1,18 @@
 class Solution:
-    def findMaxLength(self, nums: List[int]) -> int: 
+    def findMaxLength(self, nums: List[int]) -> int:
         freq = {}
         freq[0] = -1
-        ans = count = 0
+        count = ans = 0
 
         for i in range(len(nums)):
             if nums[i] == 1:
                 count += 1
             else:
                 count -= 1
-
+            
             if count in freq:
                 ans = max(ans, i - freq[count])
             else:
                 freq[count] = i
-
-        return ans
         
+        return ans
