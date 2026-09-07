39501dc169d91cfa2ba6a752c4640c160ef95d55
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-09-07 14:06:59 -0700

    1570-FINAL-PRICES-WITH-A-SPECIAL-DISCOUNT-IN-A-SHOP Stats: Time: 1 ms (64.6%), Space: 19.3 MB (31.67%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -3,15 +3,14 @@ class Solution:
         # store index of prices in stack
         # if prices[top of stack] is >= current price, pop stack and update values by subtracting.
         # keep appending index if elem is not greater than the next.
-        
-        res = prices[:]
+
         stack = []
 
         for i in range(len(prices)):
             while stack and prices[stack[-1]] >= prices[i]:
-                res[stack.pop()] -= prices[i]
+                prices[stack.pop()] -= prices[i]
 
             stack.append(i)
 
-        return res
+        return prices
         
