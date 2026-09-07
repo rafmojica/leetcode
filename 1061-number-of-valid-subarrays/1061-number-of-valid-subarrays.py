9e8282c55e41f405631e6f0c9cfcbe2a17c30144
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-09-07 16:03:35 -0700

    1061-NUMBER-OF-VALID-SUBARRAYS Stats: Time: 58 ms (29.71%), Space: 23.1 MB (73.14%) - LeetHub

--- 1061-number-of-valid-subarrays/1061-number-of-valid-subarrays.py
+++ 1061-number-of-valid-subarrays/1061-number-of-valid-subarrays.py
@@ -1,18 +1,21 @@
 class Solution:
     def validSubarrays(self, nums: List[int]) -> int:
-        # monotonic stack
+        # monotonic stack (hardest question i've done so far. how does one come up with this algorithm...)
+        
         stack = []
         ans = 0
 
         for i, num in enumerate(nums):
+            # calculate for subarrays with the largest elements in nums 
             while stack and nums[stack[-1]] > num:
                 ans += i - stack[-1]
                 stack.pop()
             
             stack.append(i)
 
+        # stack now contains indices to elements in nums that are non-decreasing
         while stack:
-            ans += len(nums) - stack[-1]
-            stack.pop()
+            ans += len(nums) - stack[-1] # calculate remaining subarrays by subtracting len(nums) from the largest element first.
+            stack.pop() # remove largest element from the right
 
-        return ans        
+        return ans
