7cc9c29f0967458ff8a0c0d4418979e5824256d9
rafa mojica <110359958+rafmojica@users.noreply.github.com>
2026-09-10 17:27:43 -0700

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 20.6 MB (7.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # create empty hashmap
-        # O(n)
         prevMap = {}
-        
-        for i in range(len(nums)):
-            difference = target - nums[i]
-            if difference in prevMap:
-                return [prevMap[difference], i]
-            prevMap[nums[i]] = i
-        return []
-        
+
+        for i, v in enumerate(nums):
+            diff = target - v
+
+            if diff in prevMap:
+                res = [prevMap[diff], i]
+                break
+                
+            prevMap[v] = i
+
+        return res
